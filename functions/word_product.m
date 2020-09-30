00f24f2a04494c8618d836e97fc601252b7a7cce
Ahmed Nazir <AhmedNazir@users.noreply.github.com>
2020-09-30 16:07:46 +0600

    Update word_product.m

--- functions/word_product.m
+++ functions/word_product.m
@@ -1,20 +1,9 @@
-function product = word_product(string)
-    
-    small_alphabet = char(97:122);
-    large_alphabet = char(65:90);
-    
-    product = 1;
+function p = word_product(s)
+s = lower(s);
 
-    alphabet_value = strings(length(small_alphabet),2);
-    
-    for i = 1:length(small_alphabet)
-        alphabet_value{i,1} = small_alphabet(i);
-        alphabet_value{i,2} = large_alphabet(i);
-    end
+p = 1;
+for i = 1: length(s)
+    p = p * (s(i) - 'a' + 1);
+end
 
-    for k = 1:length(string)
-        [order,~] = find(strcmp(alphabet_value, string(k)));
-        product = product*order;
-    end
-        
 end
